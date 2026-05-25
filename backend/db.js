af4d533dec1cdb01f99ffddac17707af2a539284
doyche07 <geymspang00@gmail.com>
2026-05-25 12:26:24 +0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
-async function connectDB() {
+const connectDB = async () => {
   try {
+
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected');
+
+    console.log('MongoDB Connected');
+
   } catch (error) {
+
     console.error('MongoDB connection failed:', error.message);
+
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
